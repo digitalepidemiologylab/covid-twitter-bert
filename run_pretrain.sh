6ea3d8bfd04b59d4ecf27ae6bc84caad3cb5e4b8
Martin Müller <martin.mathias.mueller@gmail.com>
2020-04-15 13:57:40 +0000

    updated

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
 TPU_ADDRESS=10.217.209.114
-RUN_NAME=pretrain_run1
+RUN_NAME=run1
 PROJECT_BUCKET=gs://cb-tpu-projects/covid-bert
 PRETRAINED_MODEL=gs://cloud-tpu-checkpoints/bert/uncased_L-24_H-1024_A-16
 
 PYTHONPATH="$(pwd)/tensorflow_models" python ./tensorflow_models/official/nlp/bert/run_pretraining.py \
-  --input_files ${PROJECT_BUCKET}/pretrain/pretrain_anonymized_bert_train_000.txt.tfrecords	\
-  --max_seq_length 92 \
-  --num_steps_per_epoch 100 \
+  --input_files ${PROJECT_BUCKET}/pretrain/pretrain_data/pretrain_anonymized_bert_train_000.txt.tfrecords	\
+  --max_seq_length 96 \
+  --max_predictions_per_seq 14 \
+  --num_steps_per_epoch 10 \
   --num_train_epochs 1 \
   --train_batch_size 32 \
   --tpu grpc://${TPU_ADDRESS}:8470 \
   --distribution_strategy tpu \
   --model_dir ${PROJECT_BUCKET}/${RUN_NAME}/checkpoints \
   --init_checkpoint ${PRETRAINED_MODEL}/bert_model.ckpt \
-  --model_export_path ${PROJECT_BUCKET}/${RUN_NAME} \
+  --model_export_path ${PROJECT_BUCKET}/pretrain/runs/${RUN_NAME}/ \
   --bert_config_file ${PRETRAINED_MODEL}/bert_config.json 
 
 # --helpfull output
