f80f47d6b134c20d3a5db499e92a4b3907a7eba7
Martin Müller <martin.mathias.mueller@gmail.com>
2020-04-15 09:24:19 +0200

    add test tokenize

---
+++ test_tokenize.py
@@ -0,0 +1,30 @@
+from tensorflow_models.official.nlp.bert import tokenization
+import argparse
+import os
+
+vocab_file = os.path.join('vocabs', 'bert-large-uncased-vocab.txt')
+
+def main(args):
+    tknzr = tokenization.FullTokenizer(vocab_file=args.vocab_file, do_lower_case=True)
+    print('Input:')
+    print(args.input)
+    print('\nTokenized:')
+    tokenized = tknzr.tokenize(args.input)
+    print(tokenized)
+    print('\nTokenized and converted to IDs:')
+    ids = tknzr.convert_tokens_to_ids(tokenized)
+    print(ids)
+    print('\nDirectly converted to IDs:')
+    input_ids = tknzr.convert_tokens_to_ids(args.input)
+    print(input_ids)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', required=True, type=str, help="Input to tokenize")
+    parser.add_argument('--vocab_file', default=vocab_file, help="Path to vocab file")
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
