808a05e73d4940718e5b1d053859f1c9cb45b501
Per Egil Kummervold <per@capia.no>
2020-04-18 15:04:12 +0200

    small fix

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -128,29 +128,32 @@ class ClassificationData:
 
 def create_model(max_seq_len, bert_ckpt_file):
 
-  with tf.io.gfile.GFile(bert_config_file, "r") as reader:
-      bc = StockBertConfig.from_json_string(reader.read())
-      bert_params = map_stock_config_to_params(bc)
-      bert_params.adapter_size = None
-      bert = BertModelLayer.from_params(bert_params, name="bert")
-        
-  input_ids = keras.layers.Input(shape=(max_seq_len, ), dtype='int32', name="input_ids")
-  bert_output = bert(input_ids)
+    with tf.io.gfile.GFile(bert_config_file, "r") as reader:
+        bc = StockBertConfig.from_json_string(reader.read())
+        bert_params = map_stock_config_to_params(bc)
+        bert_params.adapter_size = None
+        bert = BertModelLayer.from_params(bert_params, name="bert")
+            
+    input_ids = keras.layers.Input(shape=(max_seq_len, ), dtype='int32', name="input_ids")
+    bert_output = bert(input_ids)
 
-  print("bert shape", bert_output.shape)
+    ##Martin - Change to logging
+    print("bert shape", bert_output.shape)
 
-  cls_out = keras.layers.Lambda(lambda seq: seq[:, 0, :])(bert_output)
-  cls_out = keras.layers.Dropout(0.5)(cls_out)
-  logits = keras.layers.Dense(units=768, activation="tanh")(cls_out)
-  logits = keras.layers.Dropout(0.5)(logits)
-  logits = keras.layers.Dense(units=len(classes), activation="softmax")(logits)
+    cls_out = keras.layers.Lambda(lambda seq: seq[:, 0, :])(bert_output)
+    cls_out = keras.layers.Dropout(0.5)(cls_out)
+    logits = keras.layers.Dense(units=768, activation="tanh")(cls_out)
+    logits = keras.layers.Dropout(0.5)(logits)
+    
+    ##Martin - classed needs to be changed here
+    logits = keras.layers.Dense(units=len(classes), activation="softmax")(logits)
 
-  model = keras.Model(inputs=input_ids, outputs=logits)
-  model.build(input_shape=(None, max_seq_len))
+    model = keras.Model(inputs=input_ids, outputs=logits)
+    model.build(input_shape=(None, max_seq_len))
 
-  load_stock_weights(bert, bert_ckpt_file)
-        
-  return model
+    load_stock_weights(bert, bert_ckpt_file)
+            
+    return model
 
 
 ##############################
