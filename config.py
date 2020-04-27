8d476369f171d72d0ac3bc87560f78ccf17910f1
Martin Müller <martin.mathias.mueller@gmail.com>
2020-04-27 22:54:42 +0000

    fix paths in config

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ PRETRAINED_MODELS = {
         'bert_large_uncased': {
             'location': 'pretrained_models/bert/keras_bert/uncased_L-24_H-1024_A-16',
             'vocab_file': 'bert-large-uncased-vocab.txt',
-            'lower_case': True, 
+            'lower_case': True,
             'do_whole_word_masking': False
             },
         'bert_large_uncased_wwm': {
-            'location': 'pretrained_models/bert/keras_bert/uncased_L-24_H-1024_A-16',
-            'vocab_file': 'bert-large-uncased-vocab.txt',
+            'location': 'pretrained_models/bert/keras_bert/wwm_uncased_L-24_H-1024_A-16',
+            'vocab_file': 'bert-large-uncased-whole-word-masking-vocab.txt',
             'lower_case': True,
             'do_whole_word_masking': True
             }
