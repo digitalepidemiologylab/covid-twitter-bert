790f86f95da2546527eace7932c0290269a84a30
Martin Müller <martin.mathias.mueller@gmail.com>
2020-04-27 23:45:10 +0000

    update pretrain run files

--- run_all_pretrain_2.sh
+++ run_all_pretrain_2.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 TPU_IP=10.213.45.170
-
 python run_pretrain.py \
   --pretrain_data v1 \
   --run_prefix test \
@@ -15,21 +14,22 @@ python run_pretrain.py \
   --tpu_ip $TPU_IP \
   --steps_per_loop 1000
 
-# optional arguments:
 #   -h, --help            show this help message and exit
-#   --run_prefix RUN_PREFIX
-#                         Prefix to be added to all runs. Useful to group runs
-#                         (default: None)
+#   --tpu_ip TPU_IP       IP-address of the TPU (default: None)
 #   --pretrain_data PRETRAIN_DATA
 #                         Folder which contains pretrain data. Should be located
 #                         under gs://{bucket_name}/{project_name}/pretrain/pretr
-#                         ain_data/ (default: v1)
+#                         ain_data/ (default: None)
+#   --run_prefix RUN_PREFIX
+#                         Prefix to be added to all runs. Useful to group runs
+#                         (default: None)
+#   --model_class {bert_large_uncased,bert_large_uncased_wwm}
+#                         Model class to use (default: bert_large_uncased_wwm)
 #   --bucket_name BUCKET_NAME
 #                         Bucket name (default: cb-tpu-projects)
 #   --project_name PROJECT_NAME
 #                         Name of subfolder in Google bucket (default: covid-
 #                         bert)
-#   --tpu_ip TPU_IP       IP-address of the TPU (default: 10.74.219.210)
 #   --not_use_tpu         Do not use TPUs (default: False)
 #   --num_gpus NUM_GPUS   Number of GPUs to use (default: 1)
 #   --optimizer_type {adamw,lamb}
@@ -53,9 +53,8 @@ python run_pretrain.py \
 #                         Maximum predictions per sequence_output. (default: 14)
 #   --dtype {fp32,bf16,fp16}
 #                         Data type (default: fp32)
-#   --model_class {bert}  Model class (default: bert)
-#   --model_type {large_uncased,base_uncased}
-#                         Model class to pretraining (default: large_uncased)
 #   --steps_per_loop STEPS_PER_LOOP
 #                         Steps per loop (default: 10)
-
+#   --time_history_log_steps TIME_HISTORY_LOG_STEPS
+#                         Frequency with which to log timing information with
+#                         TimeHistory. (default: 1000)
