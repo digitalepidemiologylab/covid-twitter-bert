3697d8772fbd9ed10bc444fe93fbc6bcef0d0783
Martin Müller <martin.mathias.mueller@gmail.com>
2020-07-12 14:27:24 +0200

    v2 plotting scripts

--- analysis/fig_2.py
+++ analysis/fig_2.py
@@ -15,29 +15,27 @@ logger = logging.getLogger(__name__)
 
 @plot
 def main(args):
-    df = get_run_logs(pattern=args.run_prefix, bucket_name=args.bucket_name)
+    df = get_run_logs(pattern=args.run_prefix, project_name=args.project_name, bucket_name=args.bucket_name)
     # due to multiple runs that have been run with the same prefix
     if len(df) == 0:
         logger.info('No run logs found')
         sys.exit()
-    # filtering
-    df = df[df.created_at < '2020-05-08 06:00:00']
-    df = df[df.init_checkpoint_index.isin([0, 4, 8, 12, 16, 20])]
     # make run names nicer
     df.finetune_data = df.finetune_data.apply(lambda s: s.split('/')[-1])
-    df = df[df.finetune_data.isin(['maternal_vaccine_stance_lshtm', 'covid_category', 'twitter_sentiment_semeval', 'vaccine_sentiment_epfl', 'SST-2'])]
     df = df[['init_checkpoint_index', 'finetune_data', args.metric]]
+    df = df.dropna(subset=['init_checkpoint_index'])
 
     for finetune_data, grp in df.groupby('finetune_data'):
         mean_base = grp[grp.init_checkpoint_index == 0].mean()[args.metric]
         df.loc[df.finetune_data == finetune_data, 'metric_potential'] = (df.loc[df.finetune_data == finetune_data, args.metric] - mean_base)/(1 - mean_base)
 
     # convert checkpoint_index to steps
-    df['init_checkpoint_index'] = df.init_checkpoint_index.apply(lambda s: s*25000)
+    # df['init_checkpoint_index'] = df.init_checkpoint_index.apply(lambda s: s*25000)
+    df = df[df[args.metric] > .5]
 
     # convert dataset names
-    convert_dataset_names = {'maternal_vaccine_stance_lshtm': 'Maternal Vaccine Stance (MVS)', 'covid_category': 'COVID-19 Category (CC)', 'twitter_sentiment_semeval': 'SemEval 2016 (SE)', 'vaccine_sentiment_epfl': 'Vaccine Sentiment (VS)', 'SST-2': 'Stanford Sentiment Treebank (SST-2)'}
-    df['finetune_data'] = df.finetune_data.apply(lambda s: convert_dataset_names[s])
+    # convert_dataset_names = {'maternal_vaccine_stance_lshtm': 'Maternal Vaccine Stance (MVS)', 'covid_category': 'COVID-19 Category (CC)', 'twitter_sentiment_semeval': 'SemEval 2016 (SE)', 'vaccine_sentiment_epfl': 'Vaccine Sentiment (VS)', 'SST-2': 'Stanford Sentiment Treebank (SST-2)'}
+    # df['finetune_data'] = df.finetune_data.apply(lambda s: convert_dataset_names[s])
 
     # plotting
     height = 2.6
@@ -57,11 +55,11 @@ def main(args):
 
 def parse_args():
     parser = ArgParseDefault()
-    parser.add_argument('--bucket_name', required=True, help='Bucket name')
-    parser.add_argument('--project_name', default='covid-bert', help='Project name')
-    parser.add_argument('--run_prefix', default='eval_wwm_v4', help='Run prefix')
+    parser.add_argument('--bucket_name', default='cb-tpu-us-central1', help='Bucket name')
+    parser.add_argument('--project_name', default='covid-bert-v2', help='Project name')
+    parser.add_argument('--run_prefix', default='ct_bert_v2_eval', help='Run prefix')
     parser.add_argument('--metric', default='f1_macro', help='Metric to plot')
-    parser.add_argument('-v', '--version', type=int, default=2, help='Plot version')
+    parser.add_argument('-v', '--version', type=int, default=1, help='Plot version')
     args = parser.parse_args()
     return args
 
--- analysis/plot_metrics_by_checkpoint.py
+++ analysis/plot_metrics_by_checkpoint.py
@@ -9,16 +9,34 @@ from utils.misc import ArgParseDefault
 import ast
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)-5.5s] [%(name)-12.12s]: %(message)s')
+    # df.loc[df.init_checkpoint_index.isna(), 'init_checkpoint'] = 'ct-bert-v1'
 logger = logging.getLogger(__name__)
 
 @plot
 def main(args):
-    df = get_run_logs(pattern=args.run_prefix)
+    df = get_run_logs(pattern=args.run_prefix, bucket_name=args.bucket_name, project_name=args.project_name)
+    df.loc[(df.model_class == 'covid-twitter-bert'), 'init_checkpoint'] = 'ct-bert-v1/bla'
+    df.loc[(df.model_class != 'covid-twitter-bert') & (df.init_checkpoint_index == 0), 'init_checkpoint'] = 'bert-large-uncased-wwm/bla'
+    df = df.reset_index(drop=True)
+    df = df[df.init_checkpoint_index.isin([None, 0, 9])]
+    n = 3
+    to_delete = []
+    for (finetune_data, init_checkpoint), grp in df.groupby(['finetune_data', 'init_checkpoint']):
+        sorted_vals = grp[args.metric].sort_values(ascending=False)
+        to_delete.extend(sorted_vals[n:].index.tolist())
+
+    df = df.drop(index=to_delete)
+    df.finetune_data = df.finetune_data.apply(lambda s: s.split('/')[-1])
+    df = df[~df.finetune_data.isin(
+        ['SemEval2016_6_climate_change_is_a_real_concern', 'SemEval2016_6_feminist_movement', 'SemEval2016_6_hillary_clinton', 'SemEval2016_6_legalization_of_abortion']
+        )]
+    df['exp'] = df.init_checkpoint.apply(lambda s: s.split('/')[0])
     if len(df) == 0:
         logger.info('No run logs found')
         sys.exit()
-    fig = sns.catplot(x='finetune_data', y=args.metric, hue='init_checkpoint_index', data=df, kind='bar', height=4, aspect=1.5)
-    fig.set_xticklabels(rotation=45)
+    fig = sns.catplot(y=args.metric, x='finetune_data', hue='exp', data=df, kind='bar', height=4, aspect=1.5, ci='sd')
+    # plt.legend(loc='bottom')
+    # fig.set_xticklabels(rotation=90)
     fig.set(ylim=(0.5, 1))
     plt.gca().set_title(args.run_prefix)
 
@@ -27,9 +45,11 @@ def main(args):
 
 def parse_args():
     parser = ArgParseDefault()
-    parser.add_argument('--run_prefix', default='eval_wwm_v2', help='Prefix to plot heatmap')
-    parser.add_argument('--metric', default='accuracy', help='Metric to plot')
-    parser.add_argument('-v', '--version', type=int, default=10, help='Plot version')
+    parser.add_argument('--bucket_name', default='cb-tpu-us-central1', help='Bucket name')
+    parser.add_argument('--project_name', default='covid-bert-v2', help='Project name')
+    parser.add_argument('--run_prefix', default='ct_bert_v2_eval', help='Run prefix')
+    parser.add_argument('--metric', default='f1_macro', help='Metric to plot')
+    parser.add_argument('-v', '--version', type=int, default=7, help='Plot version')
     args = parser.parse_args()
     return args
 
