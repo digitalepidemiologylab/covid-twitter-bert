516018e74f6d4fddf5ac00fb0f06e220a7ebe100
Martin Müller <martin.mathias.mueller@gmail.com>
2020-08-03 10:13:34 +0200

    pretrain metrics helpers

--- analysis/utils/analysis_helpers.py
+++ analysis/utils/analysis_helpers.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 import json
 import os
 from functools import wraps
+from tensorflow.python.summary.summary_iterator import summary_iterator
+from tensorflow.python.framework import tensor_util
 from matplotlib.ticker import FormatStrFormatter
 import matplotlib.dates as mdates
 import logging
 import glob
 import pandas as pd
+from collections import defaultdict
+import re
 
 
 logger = logging.getLogger(__name__)
@@ -30,7 +34,8 @@ def get_run_logs(pattern=None, run_type='finetune', bucket_name='my-bucket', pro
     f_names = glob.glob(os.path.join(find_project_root(), 'data', bucket_name, project_name, run_type, '*', 'run_logs.json'))
     df = []
     for f_name in f_names:
-        if pattern is None or pattern in f_name.split('/')[-2]:
+        run_name = f_name.split('/')[-2]
+        if pattern is None or re.search(pattern, run_name):
             with open(f_name, 'r') as f:
                 df.append(json.load(f))
     df = pd.DataFrame(df)
@@ -39,13 +44,29 @@ def get_run_logs(pattern=None, run_type='finetune', bucket_name='my-bucket', pro
         df.sort_values('created_at', inplace=True, ascending=True)
     return df
 
-def get_summary_files(pattern=None, run_type='pretrain', bucket_name='my-bucket', project_name='covid-bert'):
-    f_names = glob.glob(os.path.join(find_project_root(), 'data', bucket_name, project_name, run_type, '*', 'summaries', 'train', '*'))
+def get_summary_logs(pattern=None, dataset_type='train', bucket_name='my-bucket', project_name='covid-bert'):
+    f_names = glob.glob(os.path.join(find_project_root(), 'data', bucket_name, project_name, 'pretrain', '*', 'summaries', dataset_type, '*'))
     files = []
     for f_name in f_names:
-        if pattern is None or pattern in f_name.split('/')[-4]:
+        run_name = f_name.split('/')[-4]
+        if pattern is None or re.search(pattern, run_name):
             files.append(f_name)
-    return files
+    if len(files) == 0:
+        return pd.DataFrame()
+    df = pd.DataFrame()
+    for f_name in files:
+        run_name = f_name.split('/')[-4]
+        summary_data = defaultdict(dict)
+        for e in summary_iterator(f_name):
+            for v in e.summary.value:
+                if v.simple_value:
+                    summary_data[v.tag].update({int(e.step): float(v.simple_value)})
+                else:
+                    summary_data[v.tag].update({int(e.step): float(tensor_util.MakeNdarray(v.tensor))})
+        summary_data = pd.DataFrame(summary_data)
+        summary_data['run_name'] = run_name
+        df = pd.concat([df, summary_data], axis=0)
+    return df
 
 def find_project_root():
     return os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '..', '..'))
