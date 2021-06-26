7dc168132a198f37eabd46873b1cff4ea05ce5d1
Per E Kummervold <per@capia.no>
2021-06-26 16:42:30 +0200

    config

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ PRETRAINED_MODELS = {
             'do_whole_word_masking': True,
             'strip_accents':False
             },
-        'bert_base_norwegian_cased': {
+         'bert_base_norwegian_cased': {
             'bucket_location': '',
             'hub_url': 'tensorflow/bert_multi_cased_L-12_H-768_A-12/2',
             'config': 'bert_config_base_norwegian_cased.json',
@@ -30,10 +30,20 @@ PRETRAINED_MODELS = {
             'do_whole_word_masking': True,
             'strip_accents': False
             },
-        'bert_base_scandinavian_uncased': {
+        'bert_base_norwegian_supercased': {
             'bucket_location': '',
             'hub_url': 'tensorflow/bert_multi_cased_L-12_H-768_A-12/2',
             'config': 'bert_config_base_norwegian_cased.json',
+            'is_tfhub_model': True,
+            'vocab_file': 'vocab.txt',
+            'lower_case': True,
+            'do_whole_word_masking': True,
+            'strip_accents': False
+            },
+        'bert_base_scandinavian_uncased': {
+            'bucket_location': '',
+            'hub_url': 'tensorflow/bert_multi_cased_L-12_H-768_A-12/2',
+            'config': 'bert_config_base_scandinavian_uncased.json',
             'is_tfhub_model': True,
             'vocab_file': 'scandinavian_uncased_vocab.txt',
             'lower_case': True,
